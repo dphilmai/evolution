2ee3db8c1584323cc950161fc12fa732da7f3273
dphillap <dmairhofer94@gmail.com>
2017-05-20 10:53:51 +0200

    General externality form

--- evol.py
+++ evol.py
@@ -1,4 +1,5 @@
 from scipy.integrate import odeint
+from scipy.optimize import newton
 import numpy as  np
 import matplotlib.pyplot as plt
 t = np.linspace(0,10,100)
@@ -12,26 +13,47 @@ def repaug(x,t,a1,a2,eps):
     return (x**2)*(a1+2*a2+3*eps) - (x**3)*(a1+a2+2*eps) - x*(a2+eps)
 def repaug1(x,t,a1,a2,eps):
     return (x**2)*(a1+2*a2+eps*x) - (x**3)*(a1+a2+eps*x) - x*(a2)
-def repaug2(x,t,a1,a2,eps):
-    return (x**2)*(a1+2*a2+eps*x**2)-(x**3)*(a1+a2+eps*x**2)-x*(a2)
+def repaug2(x,t,a1,a2,eps,gamma):
+    return (x**2)*(a1+2*a2+eps*x**gamma)-(x**3)*(a1+a2+eps*x**gamma)-x*(a2)
+
+def repgen(x,t,a1,a2,eps,delta,gamma1,gamma2):
+    return (x**2)*(a1+eps*x**gamma1+2*(a2+delta*x**gamma2)) - (x**3)*(a1+eps*x**gamma1+a2+delta*x**gamma2) - x*(a2+delta*x**gamma2)
+def repgenroot(x):
+    a =5
+    b =0
+    c =4
+    d =3
+    gamma1=2
+    gamma2=1
+    delta=1
+    eps=1
+    a1 = a-c
+    a2 = d-b  
+    return (x**2)*(a1+eps*x**gamma1+2*(a2+delta*x**gamma2)) - (x**3)*(a1+eps*x**gamma1+a2+delta*x**gamma2) - x*(a2+delta*x**gamma2)
 a =5
 b =0
 c =4
 d =3
+gamma1=2
+gamma2=1
+delta=1
 eps=1
 a1 = a-c
 a2 = d-b  
 x = np.linspace(0,1,100)
-plt.plot(t,repaug2(x,t,a1,a2,eps))
+root = newton(repgenroot,0.8)
+print(root)
+plt.plot(x,repgen(x,t,a1,a2,eps,delta,gamma1,gamma2))
 plt.show()
 #for i in range(1,10):
 #    x0 = i/10
 #    sol = odeint(repdyn,x0,t,args=(a1,a2))
 #    plt.plot(t,sol)
 #plt.show()
-for i in range(1,100):
-    x0 = i/100
-    sol = odeint(repaug2,x0,t,args=(a1,a2,eps))
+runs=10
+for i in range(1,runs):
+    x0 = i/runs
+    sol = odeint(repgen,x0,t,args=(a1,a2,eps,gamma1,gamma2,delta))
     plt.plot(t,sol)
 plt.plot(t,sol)
 plt.show()
