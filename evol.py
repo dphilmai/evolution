5070c58ceb284b55782bda7a05226ddb0670c927
dphillap <dmairhofer94@gmail.com>
2017-05-24 13:54:18 +0200

    Some fancy changes

--- evol.py
+++ evol.py
@@ -1,5 +1,10 @@
 from scipy.integrate import odeint
 from scipy.optimize import newton
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
 import numpy as  np
 import matplotlib.pyplot as plt
 t = np.linspace(0,10,100)
@@ -30,6 +35,7 @@ def repgenroot(x):
     a1 = a-c
     a2 = d-b  
     return (x**2)*(a1+eps*x**gamma1+2*(a2+delta*x**gamma2)) - (x**3)*(a1+eps*x**gamma1+a2+delta*x**gamma2) - x*(a2+delta*x**gamma2)
+fs=15
 a =5
 b =0
 c =4
@@ -44,6 +50,13 @@ x = np.linspace(0,1,100)
 root = newton(repgenroot,0.5)
 print(root)
 plt.plot(x,repgen(x,t,a1,a2,eps,delta,gamma1,gamma2))
+axis=plt.gca()
+axis.set_xlim([0,1])
+plt.axhline(y=0,c='0.7',ls='dashed')
+plt.axvline(x=root,c='0.7',ls='dashed')
+plt.xlabel(r'$x$',fontsize=fs)
+plt.ylabel(r'$\varphi(x)$',fontsize=fs,rotation=0)
+axis.yaxis.set_label_coords(x=-0.1,y=0.9)
 plt.show()
 #for i in range(1,10):
 #    x0 = i/10
@@ -55,6 +68,14 @@ for i in range(1,runs):
     x0 = i/runs
     sol = odeint(repgen,x0,t,args=(a1,a2,eps,delta,gamma1,gamma2))
     plt.plot(t,sol)
-sol =odeint(repgen,root,t,args=(a1,a2,eps,delta,gamma1,gamma2))
-plt.plot(t,sol)
+#sol =odeint(repgen,root,t,args=(a1,a2,eps,delta,gamma1,gamma2))
+#plt.plot(t,sol)
+plt.axhline(y=0.75,c="red",ls='dashed')
+plt.axhline(y=root,c="green",ls='dashed')
+axes=plt.gca()
+axes.set_xlim([0,10])
+axes.set_ylim([0,1])
+plt.xlabel(r'$t$',fontsize=fs)
+axes.yaxis.set_label_coords(x=-0.1,y=0.9)
+plt.ylabel(r'$x(t)$',fontsize=fs,rotation=0)
 plt.show()
