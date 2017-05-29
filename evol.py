4082ed3ccde086eec7c4c29c20857cbd27fc3bac
dphillap <dmairhofer94@gmail.com>
2017-05-29 13:44:30 +0200

    Organized properly

--- evol.py
+++ evol.py
@@ -21,6 +21,104 @@ def repaug1(x,t,a1,a2,eps):
 def repaug2(x,t,a1,a2,eps,gamma):
     return (x**2)*(a1+2*a2+eps*x**gamma)-(x**3)*(a1+a2+eps*x**gamma)-x*(a2)
 
+def logit(x,t,a1,a2,c,d,eps):
+    return (np.exp(((a1)*x+c)/eps))/((np.exp((-x*(a2)+d)/eps)+np.exp(((a1*x)+c)/eps))) -x
+#fs=15
+#a =9
+#b =0
+#c =8
+#d =7
+#gamma1=1
+#gamma2=0
+#delta=0
+#eps=15
+#a1 = a-c
+#a2 = d-b  
+#x = np.linspace(0,1,100)
+#root = newton(repgenroot,0.5)
+#print(root)
+#plt.plot(x,repgen(x,t,a1,a2,eps,delta,gamma1,gamma2))
+#axis=plt.gca()
+#axis.set_xlim([0,1])
+#plt.axhline(y=0,c='0.7',ls='dashed')
+#plt.axvline(x=root,c='0.7',ls='dashed')
+#plt.xlabel(r'$x$',fontsize=fs)
+#plt.ylabel(r'$\varphi(x)$',fontsize=fs,rotation=0)
+#axis.yaxis.set_label_coords(x=-0.1,y=0.9)
+#plt.show()
+##for i in range(1,10):
+##    x0 = i/10
+##    sol = odeint(repdyn,x0,t,args=(a1,a2))
+##    plt.plot(t,sol)
+##plt.show()
+#runs=10
+#for i in range(1,runs):
+#    x0 = i/runs
+#    sol = odeint(repgen,x0,t,args=(a1,a2,eps,delta,gamma1,gamma2))
+#    plt.plot(t,sol)
+##sol =odeint(repgen,root,t,args=(a1,a2,eps,delta,gamma1,gamma2))
+##plt.plot(t,sol)
+#plt.axhline(y=0.75,c="red",ls='dashed')
+#plt.axhline(y=root,c="green",ls='dashed')
+#axes=plt.gca()
+#axes.set_xlim([0,10])
+#axes.set_ylim([0,1])
+#plt.xlabel(r'$t$',fontsize=fs)
+#axes.yaxis.set_label_coords(x=-0.1,y=0.9)
+#plt.ylabel(r'$x(t)$',fontsize=fs,rotation=0)
+#plt.show()
+##runs=10
+##for i in range(1,runs):
+##    x0 = i/runs
+##    sol = odeint(logit,x0,t,args=(a1,a2,c,d,eps))
+##    plt.plot(t,sol)
+##plt.show()
+
+
+
+## Plot of the standard case with parameters a=5, b=3, c=0, d=2
+#fs=15
+#a =5
+#b =0
+#c =4
+#d =3
+#a1 = a-c
+#a2 = d-b  
+#x = np.linspace(0,1,100)
+#root = newton(poly,0.75,args=(a1,a2))
+#print(root)
+#plt.plot(x,poly(x,a1,a2))
+#axis=plt.gca()
+#axis.set_xlim([0,1])
+#axis.set_ylim([-0.5,0.5])
+#plt.axhline(y=0,c='0.7',ls='dashed')
+#plt.axvline(x=root,c='0.7',ls='dashed')
+#plt.xlabel(r'$x$',fontsize=fs)
+#plt.ylabel(r'$\varphi(x)$',fontsize=fs,rotation=0)
+#axis.yaxis.set_label_coords(x=-0.1,y=0.9)
+##plt.savefig('polynominal.pgf')
+##plt.savefig('polynominal.pdf')
+#plt.show()
+##The dyamic of the replicator equation
+#for i in range(1,10):
+#    x0 = i/10
+#    sol = odeint(repdyn,x0,t,args=(a1,a2))
+#    if (x0 <= root):
+#        plt.plot(t,sol,c='red')
+#    else:
+#        plt.plot(t,sol,c='blue')
+#axis=plt.gca()
+#plt.axhline(y=root,c='0.7',ls='dashed')
+#plt.xlabel(r'$x$',fontsize=fs)
+#plt.ylabel(r'$\varphi(x)$',fontsize=fs,rotation=0)
+#axis.yaxis.set_label_coords(x=-0.1,y=0.9)
+##plt.savefig('basinofattraction.pgf')
+##plt.savefig('basinofattraction.pdf')
+#plt.show()
+
+
+
+#### Plot of the externality model
 def repgen(x,t,a1,a2,eps,delta,gamma1,gamma2):
     return (x**2)*(a1+eps*x**gamma1+2*(a2+delta*x**gamma2)) - (x**3)*(a1+eps*x**gamma1+a2+delta*x**gamma2) - x*(a2+delta*x**gamma2)
 def repgenroot(x):
@@ -35,6 +133,7 @@ def repgenroot(x):
     a1 = a-c
     a2 = d-b  
     return (x**2)*(a1+eps*x**gamma1+2*(a2+delta*x**gamma2)) - (x**3)*(a1+eps*x**gamma1+a2+delta*x**gamma2) - x*(a2+delta*x**gamma2)
+# Plots
 fs=15
 a =5
 b =0
@@ -43,7 +142,7 @@ d =3
 gamma1=1
 gamma2=0
 delta=0
-eps=3
+eps= 3
 a1 = a-c
 a2 = d-b  
 x = np.linspace(0,1,100)
@@ -57,6 +156,7 @@ plt.axvline(x=root,c='0.7',ls='dashed')
 plt.xlabel(r'$x$',fontsize=fs)
 plt.ylabel(r'$\varphi(x)$',fontsize=fs,rotation=0)
 axis.yaxis.set_label_coords(x=-0.1,y=0.9)
+plt.savefig('polynominallinearmodel.pdf')
 plt.show()
 #for i in range(1,10):
 #    x0 = i/10
@@ -67,7 +167,10 @@ runs=10
 for i in range(1,runs):
     x0 = i/runs
     sol = odeint(repgen,x0,t,args=(a1,a2,eps,delta,gamma1,gamma2))
-    plt.plot(t,sol)
+    if (x0 <= root):
+        plt.plot(t,sol,c='red')
+    else:
+        plt.plot(t,sol,c='blue')
 #sol =odeint(repgen,root,t,args=(a1,a2,eps,delta,gamma1,gamma2))
 #plt.plot(t,sol)
 plt.axhline(y=0.75,c="red",ls='dashed')
@@ -78,4 +181,4 @@ axes.set_ylim([0,1])
 plt.xlabel(r'$t$',fontsize=fs)
 axes.yaxis.set_label_coords(x=-0.1,y=0.9)
 plt.ylabel(r'$x(t)$',fontsize=fs,rotation=0)
-plt.show()
+plt.savefig('dynamiclinearmodel.pdf')
